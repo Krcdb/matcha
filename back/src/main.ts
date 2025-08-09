6beaca2942cd931c8301b77c199caa63e2fe8b07
krcdb <memartin@student.42.fr>
2025-08-09 17:06:47 +0200

    feat: basic setup ok, front and back are setup

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConsoleLogger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    logger: new ConsoleLogger({
+      timestamp: true,
+    })
+  });
+
+  app.setGlobalPrefix("api");
+  app.enableCors({origin: true, credentials: true})
+
+  console.log(`listning on port ${process.env.BACK_PORT}`);
+  await app.listen(process.env.BACK_PORT ?? 3001);
 }
 bootstrap();
